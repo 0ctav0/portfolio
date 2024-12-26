ff04882209902cc290ef64fba3fbbbc3a7c81ff5
Anna Ra <anna.ra.phoenix@gmail.com>
2024-12-26 18:50:40 +0500

    work experience

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -27,6 +27,7 @@ export const Home = () => {
                 <p class="sub">Located in Russia, eager to relocate</p>
             </div>
             <h1>Game Developer</h1>
+
             <h2>Contacts</h2>
             <div class="horiz">
                 <a class="horiz" target="_blank" href="https://t.me/anna_gamedev"><img alt="Link to telegram" width={32}
@@ -41,10 +42,36 @@ export const Home = () => {
             </div>
             <h2>About Me</h2>
             <p>I am pretty artistic. 💃 Love to have fun, play, think of, and create video games. I listen to the podcast "The Debug Log" about Game Development.</p>
-            <p>I have 4 years of work experience as a developer, 2 of them as a frontend developer, mostly on React.</p>
+            <p>I love a lot of PC games of different genres, such as FPS, Action, RTS/TBS, Survaival-Craft. Games of those genres: MM6, Heroes III, Gothic 2, Dark Souls, Cyberpunk 2077, Total Annihilation, 7 Days to Die, Terraria.</p>
+            <p>I don't play mobile games much. Mobile games I like: Hidden City, Orna, HearthStone, PvZ 2, Rush Royale, TD Heroes.</p>
+
+            <h2>Work experience</h2>
+            <div id="work-exp">
+
+                <a target="_blank" href="https://softline.ru/">Softline</a>
+                <span>1 Year as tech specialist</span>
+                {/* <span>Groovy, SQL, JavaScript</span> */}
+                <span>Configuring ITSM platforms: Naumen ServiceDesk, ServiceNow, writing SQL to reports.</span>
+
+                <a target="_blank" href="https://sgaz.pro/">Sgaz</a>
+                <span>1 Year as fullstack dev</span>
+                {/* <span>Scala, Java, SQL</span> */}
+                <span>Developing AIS for construction. Fullstack on Scala, ORM, SQL, PostgreSQL.</span>
+
+                <a target="_blank" href="https://digitalleague.ru/">Digital League</a>
+                <span>1 Year as frontend dev</span>
+                {/* <span>Typescript, React, Git, Npm</span> */}
+                <span>Developing AISs for a couple of projects. Typescript, React, Git, Npm, Figma.</span>
+
+                <a target="_blank" href="https://alloysoftware.com/">Alloy Software</a>
+                <span>1 Year as frontend dev</span>
+                {/* <span>Typescript, React, Git, Npm</span> */}
+                <span>Developing AIS for discovering devices in a subnet. Typescript, React, Material Design, Cypress (E2E), Figma.</span>
+            </div>
+
             <h2>Skills</h2>
             <div class="horiz">
-                <span class="skill" data-tippy-content="Proficient Level. Overall 8 years. Work: 2 years as React frontend dev">TypeScript</span>
+                <span class="skill" data-tippy-content="Proficient Level. Overall 6 years">TypeScript</span>
                 <span class="skill" data-tippy-content={BASIC_LEVEL}>Unity / C#</span>
                 <span class="skill" data-tippy-content={BASIC_LEVEL}>Figma</span>
                 <span class="skill" data-tippy-content={BASIC_LEVEL}>Git</span>
@@ -52,7 +79,7 @@ export const Home = () => {
             </div>
             <br />
             <div class="horiz">
-                <span class="skill" data-tippy-content="Basic level. I actively improve it" >C++</span>
+                <span class="skill" data-tippy-content="Basic level" >C++</span>
                 <a class="badge" target="_blank" href="https://codewars.com/users/0ctav0"><img src="https://www.codewars.com/users/0ctav0/badges/micro" /></a>
             </div>
 
