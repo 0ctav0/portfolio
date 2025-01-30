6ee5d5b1483a8a6734be6b7706e456b28211954c
Anna Ra <anna.ra.phoenix@gmail.com>
2025-01-30 13:37:55 +0300

    about me, skills section are updated

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -24,7 +24,7 @@ export const Home = () => {
             <div class="me">
                 <img src={Me} alt="Anna Ra Game Developer" />
                 <h1 class="name">Anna Ra</h1>
-                <p class="sub">Located in Russia, eager to relocate</p>
+                <p class="sub">Located in Russia, eager to relocate to France</p>
             </div>
             <h1>Game Developer</h1>
 
@@ -41,11 +41,17 @@ export const Home = () => {
                     My Github</a>
             </div>
             <h2>About Me</h2>
-            <p>I am pretty artistic. 💃 Love to have fun, play, think of, and create video games. I listen to the podcast "The Debug Log" about Game Development.</p>
-            <p>I love a lot of PC games of different genres, such as FPS, Action, RTS/TBS, Survaival-Craft. Games of those genres: MM6, Heroes III, Gothic 2, Dark Souls, Cyberpunk 2077, Total Annihilation, 7 Days to Die, Terraria.</p>
-            <p>I don't play mobile games much. Mobile games I like: Hidden City, Orna, HearthStone, PvZ 2, Rush Royale, TD Heroes.</p>
+            <p>I made a telegram game <a target="_blank" href="https://t.me/ra_minesweeper_bot">Minesweeper</a>.</p>
+            <p>I love learning languages; currently I am learning French. I listen to the podcast "The Debug Log" about game development. And love to run in parks.</p>
+            <br />
+            <p>I am hardcore PC gamer.</p>
+            <p>I love a lot of PC games of different genres, such as FPS, Action, RPG, RTS/TBS, Survaival-Craft. Games of those genres:</p>
+            <p> MM6, Heroes III, Gothic 2, Dark Souls, Cyberpunk 2077, Total Annihilation, AoE2 HD, Warcraft 3, Call of Duty Black Ops 2 (with elements of turn based tactics), 7 Days to Die, Minecraft, Terraria, etc.</p>
+            <p>I don't play mobile games much. Mobile games I like: Hidden City (HOPA), Orna (GPS MMORGP), HearthStone, PvZ 2, Rush Royale (TD with cards), TD Heroes.</p>
+            <br />
+            <p>I want to enter GameDev industry with the possibility to grow into a Game Designer, or into a Graphics Programer C++, or into a 3d Modeler, to make PC hardcore AAA games.</p>
 
-            <h2>Work experience</h2>
+            <h2>Work experience (None in GameDev)</h2>
             <div id="work-exp">
 
                 <a target="_blank" href="https://softline.ru/">Softline</a>
@@ -70,16 +76,21 @@ export const Home = () => {
             </div>
 
             <h2>Skills</h2>
+            <p>In my work I like to use such tools:</p>
+            <p>I use <span class="skill" data-tippy-content={"Basic level, I can cherry pick redundant commit with help of tutorial. I understand the concept of atomic commits and try to follow"}>Git</span> on a daily basis to commit to Github, when working on some projects</p>
+            <p>I am learning <span class="skill" data-tippy-content={"Making a donut by some tutorial!"}>Blender</span></p>
+            <p>I created several screens in <span class="skill" data-tippy-content={"I also used it before in my work as a Frontend Dev, to build web interfaces"}>Figma</span> for my pet project game</p>
+            <p>I love to use <span class="skill" data-tippy-content={"For organizing knowledge and making a game design of my ideas"}>Xmind</span></p>
+            <p>Use <span class="skill" data-tippy-content={"Many, many years of basic usage"}>Paint.net</span></p>
+            <br />
             <div class="horiz">
                 <span class="skill" data-tippy-content="Proficient Level. Overall 6 years">TypeScript</span>
                 <span class="skill" data-tippy-content={BASIC_LEVEL}>Unity / C#</span>
-                <span class="skill" data-tippy-content={BASIC_LEVEL}>Figma</span>
-                <span class="skill" data-tippy-content={BASIC_LEVEL}>Git</span>
                 <span class="skill" data-tippy-content={BASIC_LEVEL}>SQL</span>
             </div>
             <br />
-            <div class="horiz">
-                <span class="skill" data-tippy-content="Basic level" >C++</span>
+            <div class="horiz">Practicing in
+                <span class="skill" data-tippy-content="To grow into something bigger" >C++</span>
                 <a class="badge" target="_blank" href="https://codewars.com/users/0ctav0"><img src="https://www.codewars.com/users/0ctav0/badges/micro" /></a>
             </div>
 
